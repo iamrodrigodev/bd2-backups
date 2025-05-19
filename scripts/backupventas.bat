d6d89c9e2718ee1a8dec57ebd30f04581342ec79
rodrigodev.ts <rodrigodev.ts@gmail.com>
2025-05-19 13:44:54 -0500

    update

--- scripts/backupventas.bat
+++ scripts/backupventas.bat
@@ -10,8 +10,8 @@ for /f "tokens=1-4 delims=/: " %%a in ('wmic os get localdatetime ^| find "."') 
 )
 set BACKUP_DATE=!CURRENT_DATE:~0,4!-!CURRENT_DATE:~4,2!-!CURRENT_DATE:~6,2!_!CURRENT_DATE:~8,2!-!CURRENT_DATE:~10,2!-!CURRENT_DATE:~12,2!
 
-set BACKUP_FILE=!BACKUP_PATH!\!DB_NAME!_backup_!BACKUP_DATE!.bak
+sqlcmd -S %SQL_SERVER% -Q "BACKUP DATABASE [!DB_NAME!] TO DISK = 'C:\BackupsVentasBD2\!DB_NAME!_backup_!BACKUP_DATE!.bak' WITH FORMAT, MEDIANAME = 'VentasBD2_Backups', NAME = 'Backup de !DB_NAME! del !BACKUP_DATE!';" -t 30 2>NUL
 
-sqlcmd -S %SQL_SERVER% -Q "BACKUP DATABASE [!DB_NAME!] TO DISK = 'C:\BackupsVentasBD2\!DB_NAME!_backup_!BACKUP_DATE!.bak' WITH FORMAT, MEDIANAME = 'VentasBD2_Backups', NAME = 'Backup de !DB_NAME! del !BACKUP_DATE!';" -t 30
-
+set BACKUP_FILE=!DB_NAME!_backup_!BACKUP_DATE!.bak
+echo Se creo el backup exitosamente: !BACKUP_FILE!
 pause
